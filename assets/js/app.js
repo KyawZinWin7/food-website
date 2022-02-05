8fa30835e4163481358a7f463a59dbb1c644bc1f
KyawZinWin7 <kyawzinwin.beec@gmail.com>
2022-02-05 17:48:03 +0630

    All finished

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,26 +19,28 @@ var swiper = new Swiper(".home-slider", {
 });
 
 var swiper = new Swiper(".food-slider", {
-  grabCursor: true,
-  loop: true,
-  centeredSlides: true,
+  grabCursor:true,
+  loop:true,
+  centeredSlides:true,
   spaceBetween: 20,
   pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
+     el: ".swiper-pagination",
+     clickable: true,
   },
   breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    700: {
-      slidesPerView: 2,
-    },
-    1000: {
-      slidesPerView: 3,
-    },
+     0: {
+       slidesPerView: 1,
+     },
+     700: {
+       slidesPerView: 2,
+     },
+     1000: {
+       slidesPerView: 3,
+     },
   },
 });
+
+
 
 let previewContainer = document.querySelector('.food-preview-container');
 let previewBox = previewContainer.querySelectorAll('.food-preview');
@@ -63,3 +65,45 @@ previewContainer.querySelector('#close-preview').onclick = () => {
     close.classList.remove('active');
   })
 }
+
+
+// menu
+
+var swiper = new Swiper(".menu-slider", {
+  grabCursor: true,
+  loop: true,
+  autoHeight: true,
+  centeredSlides: true,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+ 
+});
+
+
+// blogs
+
+var swiper = new Swiper(".blogs-slider", {
+  grabCursor:true,
+  loop:true,
+  centeredSlides: true,
+  autoHeight:true,
+  spaceBetween: 20,
+  pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+  },
+  breakpoints: {
+     0: {
+       slidesPerView: 1,
+     },
+     700: {
+       slidesPerView: 2,
+     },
+     1000: {
+       slidesPerView: 3,
+     },
+  },
+});
